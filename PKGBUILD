be2d26a6bc6ec26f9554dd828e9488d3c8a58a0c
Yubo Su <yubo56@gmail.com>
2016-09-14 23:09:55 -0700

    Initial commit

---
+++ PKGBUILD
@@ -0,0 +1,58 @@
+# $Id: PKGBUILD 113973 2014-07-01 10:51:04Z spupykin $
+# Maintainer: Sergej Pupykin <pupykin.s+arch@gmail.com>
+# Contributor: Dag Odenhall <dag.odenhall@gmail.com>
+# Contributor: Grigorios Bouzakis <grbzks@gmail.com>
+
+pkgname=dwmalt
+pkgver=6.0
+pkgrel=2
+pkgdesc="A dynamic window manager for X"
+url="http://dwm.suckless.org"
+arch=('i686' 'x86_64')
+license=('MIT')
+options=(zipman)
+depends=('libx11' 'libxinerama')
+install=dwmalt.install
+source=(dwmalt-$pkgver.tar.gz
+	config.h
+	dwmalt.desktop)
+
+build() {
+  cd $srcdir/$pkgname-$pkgver
+  cp $srcdir/config.h config.h
+  sed -i 's/CPPFLAGS =/CPPFLAGS +=/g' config.mk
+  sed -i 's/^CFLAGS = -g/#CFLAGS += -g/g' config.mk
+  sed -i 's/^#CFLAGS = -std/CFLAGS += -std/g' config.mk
+  sed -i 's/^LDFLAGS = -g/#LDFLAGS += -g/g' config.mk
+  sed -i 's/^#LDFLAGS = -s/LDFLAGS += -s/g' config.mk
+  make X11INC=/usr/include/X11 X11LIB=/usr/lib/X11
+}
+
+package() {
+  cd $srcdir/$pkgname-$pkgver
+  make PREFIX=/usr DESTDIR=$pkgdir install
+  install -m644 -D LICENSE $pkgdir/usr/share/licenses/$pkgname/LICENSE
+  install -m644 -D README $pkgdir/usr/share/doc/$pkgname/README
+  install -m644 -D $srcdir/dwmalt.desktop $pkgdir/usr/share/xsessions/dwmalt.desktop
+}
+md5sums=('482c9b01f1c2f8fe728610b40f88d7c9'
+         '55239b76ea3783021c422aff8c4a96c2'
+         '939f403a71b6e85261d09fc3412269ee')
+md5sums=('482c9b01f1c2f8fe728610b40f88d7c9'
+         '3e178e2a534b15f493fbc919cd0a9c50'
+         '939f403a71b6e85261d09fc3412269ee')
+md5sums=('482c9b01f1c2f8fe728610b40f88d7c9'
+         'f3cd7df806500f2704461c6a4451ef97'
+         '939f403a71b6e85261d09fc3412269ee')
+md5sums=('482c9b01f1c2f8fe728610b40f88d7c9'
+         '3b14a83cf9601c263f10360c1634b553'
+         '939f403a71b6e85261d09fc3412269ee')
+md5sums=('482c9b01f1c2f8fe728610b40f88d7c9'
+         '7e5ec11f7a4b6e96d43164377c00d06f'
+         '939f403a71b6e85261d09fc3412269ee')
+md5sums=('482c9b01f1c2f8fe728610b40f88d7c9'
+         'f86984b1d5260896bf521f4ccc3032da'
+         '939f403a71b6e85261d09fc3412269ee')
+md5sums=('482c9b01f1c2f8fe728610b40f88d7c9'
+         '68592dc8cbe5c6ad358d29cb76c1afdb'
+         '939f403a71b6e85261d09fc3412269ee')
---
+++ config.h
@@ -0,0 +1,134 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const char font[]            = "-*-terminus-medium-r-*-*-22-*-*-*-*-*-*-*";
+static const char normbordercolor[] = "#000000";
+static const char normbgcolor[]     = "#000000";
+static const char normfgcolor[]     = "#49e20e";
+static const char selbordercolor[]  = "#000000";
+static const char selbgcolor[]      = "#000000";
+static const char selfgcolor[]      = "#49e20e";
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const Bool showbar           = True;     /* False means no bar */
+static const Bool topbar            = True;     /* False means bottom bar */
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+   /* class      instance    title       tags mask     isfloating   monitor */
+   { "Pidgin",      NULL,       NULL,       1 << 7,            False,       -1 },
+   { "chromium",    NULL,       NULL,       1 << 8,            False,       -1 },
+};
+
+/* layout(s) */
+static const float mfact      = 0.5; /* factor of master area size [0.05..0.95] */
+static const int nmaster      = 1;    /* number of clients in master area */
+static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
+
+static const Layout layouts[] = {
+    /* symbol     arrange function */
+    { "-",      tile },    /* first entry is default */
+    { "~",      NULL },    /* no layout function means floating behavior */
+    { "0",      monocle },
+};
+
+/* key definitions */
+#define MODKEY Mod1Mask
+#define TAGKEYS(KEY,TAG) \
+    { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+    { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static const char *dmenucmd[] = { "dmenu_run", "-fn", "-*-terminus-medium-*-*-*-*-180-*-*-*-*-*-*", "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]  = { "xterm", NULL };
+static const char *chromium[] = { "chromium", "--auth-server-whitelist=*.caltech.edu", NULL };
+static const char *volup[] = { "volup", NULL };
+static const char *voldown[] = { "voldown", NULL };
+static const char *voltoggle[] = { "voltoggle", NULL };
+static const char *brightup[] = { "brightup", NULL };
+static const char *brightdown[] = { "brightdown", NULL };
+static const char *changekeys[] = { "changekeys", NULL };
+static const char *toggleInt[] = { "toggleInt", NULL };
+static const char *tptoggle[] = { "tptoggle", NULL };
+static const char *transtoggle[] = { "transToggle", NULL };
+static const char *compmgrToggle[] = { "compmgrToggle", NULL };
+static const char *rebg[] = { "rebg", NULL };
+/* static const char *suspend[] = { "suspend_me", NULL }; */
+static const char *gdict[] = { "goldendict", NULL };
+
+/* #define XF86Sleep 0x1008ff2f */
+static Key keys[] = {
+    /* modifier                     key        function        argument */
+    { MODKEY,                       XK_F1,     spawn,          {.v = changekeys } },
+    { MODKEY|ShiftMask,             XK_F1,     spawn,          {.v = toggleInt } },
+    { MODKEY,                       XK_F2,     spawn,          {.v = brightdown } },
+    { MODKEY,                       XK_F3,     spawn,          {.v = brightup } },
+    { MODKEY,                       XK_F4,     spawn,          {.v = transtoggle } },
+    { MODKEY|ShiftMask,             XK_F4,     spawn,          {.v = compmgrToggle } },
+    { MODKEY,                       XK_F5,     spawn,          {.v = rebg } },
+    { MODKEY,                       XK_F9,     spawn,          {.v = voldown } },
+    { MODKEY,                       XK_F10,    spawn,          {.v = volup} },
+    { MODKEY,                       XK_F11,    spawn,          {.v = voltoggle } },
+    { MODKEY,                       XK_F12,    spawn,          {.v = tptoggle } },
+    { MODKEY,                       XK_q,      spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_period, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_comma,  spawn,          {.v = chromium } },
+    { MODKEY,                       XK_u,      spawn,          {.v = gdict } },
+    /* { 0,                            XF86Sleep, spawn,          {.v = suspend } }, */
+    { MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+    { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+    { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+    { MODKEY,                       XK_h,      setmfact,       {.f = -0.02} },
+    { MODKEY,                       XK_l,      setmfact,       {.f = +0.02} },
+    { MODKEY,                       XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_Tab,    view,           {0} },
+    { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                       XK_space,  setlayout,      {0} },
+    { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+    { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+    { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+    { MODKEY,                       XK_c,      focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_r,      focusmon,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_c,      tagmon,         {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_r,      tagmon,         {.i = +1 } },
+    TAGKEYS(                        XK_1,                      0)
+    TAGKEYS(                        XK_2,                      1)
+    TAGKEYS(                        XK_3,                      2)
+    TAGKEYS(                        XK_4,                      3)
+    TAGKEYS(                        XK_5,                      4)
+    TAGKEYS(                        XK_6,                      5)
+    TAGKEYS(                        XK_7,                      6)
+    TAGKEYS(                        XK_8,                      7)
+    TAGKEYS(                        XK_9,                      8)
+    { MODKEY|ShiftMask,             XK_Escape, quit,           {0} },
+};
+
+/* button definitions */
+/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
